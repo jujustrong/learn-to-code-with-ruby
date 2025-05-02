d86d72c3883586a3775a83d6fe3c2a249c5009ff
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-02 02:13:20 -0600

    completed "predicate methods in if statements" lesson

---
+++ 08 - Conditionals/predicate_methods_in_if_statements.rb
@@ -0,0 +1,10 @@
+if 5.odd?
+  #remember: predicate methods typically have a question mark at the end 
+  puts "The number is indeed odd"
+end
+
+word = "kangaroo"
+
+if word.include?("roo")
+  puts "that substring indeed exists"
+end
