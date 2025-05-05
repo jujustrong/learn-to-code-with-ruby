e4089e295a7d140b4dfcd133db3aca4c6812a87a
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-05 12:40:31 -0600

    Completed "if and elsif" exercise

---
+++ 08 - Conditionals/if_and_elsif.rb
@@ -0,0 +1,20 @@
+color = "Green"
+
+if color == "Red" 
+  puts "Red is rad"
+elsif color == "Yellow"
+  puts "Yellow is mellow"
+elsif color == "Green"
+  puts "Green is great"
+end
+
+
+number = 45
+
+if number < 25
+  puts "That is a small number"
+elsif number < 50
+  puts "That is a medium sized number"
+elsif number >= 55
+  puts "That is a big number"
+end
