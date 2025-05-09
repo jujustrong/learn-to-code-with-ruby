f3d753559b3897d8145e69e303c861872e15d772
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-09 08:50:09 -0600

    completed "string comparisons" lesson

---
+++ 09 - Strings I/string_comparisons.rb
@@ -0,0 +1,29 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+puts a == b
+puts a == c
+puts a == a
+puts b == "hello"
+puts a != b
+puts a != c
+
+puts 
+
+puts 3 < 5
+puts "A" < "B"
+puts "A" < "Z"
+puts "M" < "E"
+
+puts "L" > "F"
+puts "L" > "M"
+
+# Ruby puts all uppercase letters before lowercase letters in order of importance
+puts "Z" < "a"
+puts "a" < "z"
+puts "a" < "Z"
+
+puts 
+
+puts "hello" < "help"
