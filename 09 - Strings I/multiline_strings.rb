f0ac60e0ee821eb55002ce78d48c39618da5a44d
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-08 23:21:12 -0600

    Completed "Multiline Strings" lesson.

---
+++ 09 - Strings I/multiline_strings.rb
@@ -0,0 +1,11 @@
+# MLS is the identifier in this example (Multi Line String)
+
+poetry = <<MLS
+  What a beautiful language Ruby is
+  It allows you to wrie multiple lines like this
+  Split your words up across multiple lines
+  Everything still looks nice and fine.
+    -Shakespeare
+MLS
+
+puts poetry
