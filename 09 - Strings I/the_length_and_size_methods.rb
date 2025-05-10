47a8115f6fb6d011a050b515575a57574e7524c9
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-09 19:26:13 -0600

    completed "length and size" lesson

---
+++ 09 - Strings I/the_length_and_size_methods.rb
@@ -0,0 +1,11 @@
+puts "hello world".length
+puts "     # $ z5 9  !".length
+puts "".length
+
+puts "hello world".size
+puts "     # $ z5 9  !".size
+puts "".size
+
+
+# size can be used as the same as length
+
