b6ee07e336f324f7400c6ad3591625628eddff00
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-12 18:23:47 -0600

    completed "empty and nil" lesson

---
+++ 09 - Strings I/the_empty?_and_nil?_methods.rb
@@ -0,0 +1,12 @@
+puts "".empty?
+puts " ".empty?
+puts "abc".empty?
+
+puts
+
+puts "hello".nil? # Good for double checking objects the user gives you. Checks if they are nil
+
+puts 
+
+something = nil
+puts something.nil?
