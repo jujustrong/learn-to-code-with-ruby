603e18bfad03c787496f5924aef2786263ab567c
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-12 22:31:05 -0600

    completed "the until loop" lesson

---
+++ 10 - Loops/the_until_loop.rb
@@ -0,0 +1,10 @@
+# The inverse of the while loop 
+# similar to 'unless' and 'if'
+# a while loop executes while a condition is true
+# an until loop executes until a condition is true
+
+i = 1
+until i > 10
+  puts "Iteration: #{i}"
+  i += 1
+end
