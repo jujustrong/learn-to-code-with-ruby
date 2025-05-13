8ed33f0d173cf894bd97a4aafa1f5c06e77d6f16
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-12 18:20:04 -0600

    completed "Insert Method" lesson

---
+++ 09 - Strings I/the_insert_method.rb
@@ -0,0 +1,7 @@
+typo = "GeorgWashington"
+typo.insert(5, "e ")      # You can enter as many characters to insert as you want.
+puts typo
+
+typo = "George Washingto"
+typo.insert(-1, "n")
+puts typo
