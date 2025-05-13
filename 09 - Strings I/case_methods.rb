c34cf8cd803213bb52330550fa3aaeb8f55f93d9
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-12 18:29:30 -0600

    completed "Case Methods" lesson

---
+++ 09 - Strings I/case_methods.rb
@@ -0,0 +1,14 @@
+# THESE METHODS DO NOT CHANGE THE ORIGINAL STRING
+puts "Eiffel Tower".upcase
+puts "Eiffel Tower".downcase
+puts "Eiffel Tower".swapcase
+
+puts "eiffel tower".capitalize # Only effective when you want to capitalize the first character in the string
+puts "EIFFEL TOWER".capitalize
+
+puts 
+
+tourist_hotspot = "Eiffel Tower"
+uppercased_hotspot = tourist_hotspot.upcase
+puts tourist_hotspot
+puts uppercased_hotspot
