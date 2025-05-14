660e2ba1c496ad81bae4304aae67ae95c6fa6765
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-14 16:07:15 -0600

    completed "the debugger" lesson

---
+++ 11 - Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,15 @@
+require "debug"
+
+candy = "Sour Patch Kids"
+puts "I love eating #{candy}"
+
+binding.break
+
+beverage = "Arizona Iced Tea"
+puts "I love drinking #{beverage}"
+
+binding.break
+
+puts "This is the end of the program"
+
+# debugger and binding.break are interchangeable
