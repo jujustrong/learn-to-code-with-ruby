d944fbd70b9859220c028972b08628ccd16e7d71
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-20 17:26:55 -0600

    completed "Shorthand %w for arrays" lesson

---
+++ 12 - Arrays I/shorthand_%w_syntax_to_create_an_array_of_strings.rb
@@ -0,0 +1,8 @@
+names = ["Jack", "Jill", "John", "James" ]
+p names
+
+names = %w[ Jack Jill John James ]
+p names
+
+# %w is a shorthand syntax to create an array of strings
+# It is a shorthand for creating an array of strings without having to use quotes and commas.
