7096c67225ef6aac23a35845b44a4489d27c4e9b
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-20 21:45:37 -0600

    Completed "creating arrays" Coding challenge

---
+++ 12 - Arrays I/coding_exercise_creating_arrays.rb
@@ -0,0 +1,18 @@
+# Declare an empty_array method that returns an empty array
+def empty_array
+  empty_array = []
+end
+
+
+# Declare a three_colors method that returns an array with
+# the strings of "red", "green", and "blue" (in that order)
+def three_colors
+  three_colors = [ "red", "green", "blue" ]
+end
+
+
+# Declare a five_favorite_numbers method that returns an
+# array of 5 integers. The integers are up to you.
+def five_favorite_numbers
+  five_favorite_numbers = [ 8, 10, 17, 45, 99 ]
+end
