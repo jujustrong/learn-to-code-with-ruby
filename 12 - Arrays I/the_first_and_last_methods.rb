ee90c58be65b41167fe97809c29bae22a282cc72
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-27 21:42:05 -0600

    Completed "first and last methods" lesson

---
+++ 12 - Arrays I/the_first_and_last_methods.rb
@@ -0,0 +1,12 @@
+sushi = ["Salmon", "Tuna", "Shrimp", "Eel", "Yellowtail"]
+
+p sushi.first
+p sushi.first(4)    # You will get back an array when giving 'first' an argument
+p sushi.first(3)
+p sushi.first(1)
+
+puts 
+
+p sushi.last
+p sushi.last(3)
+p sushi.last(1)
