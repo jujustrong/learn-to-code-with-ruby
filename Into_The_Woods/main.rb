f1b5338dba53ae246b72bacf6aea43ee4e29f475
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-29 16:55:31 -0600

    added 'start' and 'first_choice' methods to main.rb

--- Into_The_Woods/main.rb
+++ Into_The_Woods/main.rb
@@ -1,17 +1,32 @@
+require_relative 'player'
+require_relative 'combat'
+require_relative 'utility'
+
 class Game
   def initialize
     clear_screen
     puts "🌲 Welcome to INTO THE WOODS 🌲"
     print "Enter your name adventurer: "
     name = gets.chomp
-    @player = Player.new(name)
+    Player.new(name)
   end
 
+  def start
+    pause_and_clear
+    puts "Hello #{Player.name}."
+    first_choice
+  end
 
-
-
-
-
-
-
+  def first_choice
+    puts "Do you go left or right?"
+    choice = gets.chomp.downcase
+    case choice
+    when "left"
+      combat(@player, "Goblin", 40)
+    when "right"
+      @player.add_item("Magic Stone")
+    end
+  end
 end
+
+Game.new
