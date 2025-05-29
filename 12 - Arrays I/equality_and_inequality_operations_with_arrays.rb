75e6c134292f94ccf114f4063590f40a6f5fb963
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-28 23:58:18 -0600

    completed "equality and inequality" lesson

---
+++ 12 - Arrays I/equality_and_inequality_operations_with_arrays.rb
@@ -0,0 +1,15 @@
+candy = ["Skittles", "Starbursts", "Snickers"]
+desserts = ["Skittles", "Starbursts", "Snickers"]
+vegetables = ["Radishes", "Onions"]
+sweets = ["Skittles", "Starbursts", "snickers"]
+
+
+puts candy == desserts # Everything in the two strings is exactly the same
+puts candy == vegetables
+puts candy == sweets
+
+puts 
+
+puts candy != desserts
+puts candy != vegetables
+puts candy != sweets
