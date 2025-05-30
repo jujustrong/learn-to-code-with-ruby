651774dc245925b4df1d888a5c62a0802e3c0113
Julian Armstrong <julian.armstrong8@gmail.com>
2025-05-30 14:30:03 -0600

    Added positive_sum practice challenge

--- 34 - Codewars/practice.rb
+++ 34 - Codewars/practice.rb
@@ -15,4 +15,24 @@ def past(h, m, s)
   ((m * 60) + (h * 3600) + s) * 1000
 end
 
-puts past(1, 1, 1)
+#---------------------------------------------------------------------------------
+
+def positive_sum(arr)   # You get an array of numbers, return the sum of all positive ones. 0 if arr is empty
+  count = 0
+  arr.each do |i|
+    if i > 0
+      count += i
+    else
+      next
+    end
+  end
+  count
+end
+
+# Alternate Solution
+
+def positive_sum(arr)
+  arr.select(&:positive?).sum
+end
+
+#---------------------------------------------------------------------------------
