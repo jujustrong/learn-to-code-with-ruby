6e5f1da210183d597e3212282d69bc8e0f57d46d
Julian Armstrong <julian.armstrong8@gmail.com>
2025-07-02 14:33:17 -0600

    completed alphabet ranges lesson

---
+++ 13 - Ranges/alphabetical_ranges.rb
@@ -0,0 +1,15 @@
+# Same rules as using numbers.
+
+alphabet = "a".."z"
+p alphabet.class
+p alphabet.first
+p alphabet.last
+p alphabet.first(3)
+p alphabet.last(5)
+
+puts
+
+# REMEMBER: Capital letters are BEFORE lowercase letters (A-Z), (a-z)
+
+alphabet = "A".."z" 
+p alphabet.first(40)
