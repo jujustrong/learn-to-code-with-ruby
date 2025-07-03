465db6690f94bd78ba9f1fa2a4443dd0f3c2aa05
Julian Armstrong <julian.armstrong8@gmail.com>
2025-07-02 20:10:20 -0600

    completed "while" and "until" iteration lesson

---
+++ 14 - Arrays II/iteration_over_array_with_while_or_until_loops.rb
@@ -0,0 +1,19 @@
+animals = ["Lion", "Zebra", "Baboon", "Cheetah"]
+
+i = 0
+
+while i < animals.length #4
+  puts i
+  puts animals[i]
+  i += 1
+end
+
+puts 
+
+i = 0
+
+until i == animals.length
+  puts i
+  puts animals[i]
+  i += 1
+end
