9c5b23bc9d4f723fe6e49a1c058f1cbc4b49cd5f
Julian Armstrong <julian.armstrong8@gmail.com>
2025-07-16 16:17:44 -0600

    Completed custom split method coding challenge

---
+++ 15 - Strings II/coding_exercise_split_method.rb
@@ -0,0 +1,35 @@
+# Define a custom_split method that accepts a piece of text and a delimiter.
+# The method should return an array of the segments of the text
+# after being split by the delimiter. Your solution should NOT
+# use the built-in split method on a string. Assume that the delimiter
+# will never be an empty string.
+#
+# Examples:
+# The => indicates the expected return value
+# custom_split("Hi, my name is Boris", " ")  => ["Hi,", "my", "name", "is", "Boris"]
+# custom_split("ravioli is delicious", "i")  => ["rav", "ol", " ", "s del", "c", "ous"]
+# custom_split("Zebra", "j")                 => ["Zebra"]
+# custom_split(" hello", " ")                => ["hello"]
+
+def custom_split(text, delimiter)
+  results = []
+  current = ""
+
+  text.each_char do |char|
+    if char == delimiter
+      results << current
+      current = ""
+    else
+      current << char
+    end
+  end
+  
+  results << current if current.length > 0
+
+  results
+end
+
+p custom_split("Hi, my name is Boris", " ")
+p custom_split("ravioli is delicious", "i")
+p custom_split("Zebra", "j")
+p custom_split(" hello", " ")
