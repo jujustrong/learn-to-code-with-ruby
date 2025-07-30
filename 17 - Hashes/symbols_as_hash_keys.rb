008869ad92aa0681cea28c32e47adf98f25d396c
Julian Armstrong <julian.armstrong8@gmail.com>
2025-07-30 10:30:37 -0600

    completed the "symbols as has keys" lesson

---
+++ 17 - Hashes/symbols_as_hash_keys.rb
@@ -0,0 +1,16 @@
+# person = {
+#   :name => "Boris", 
+#   :age => 32, 
+#   :handsome => true
+# }
+
+# This is the shorthand version of the first example. 
+person = {
+  name: "Boris", 
+  age: 32, 
+  handsome: true
+}
+
+puts person[:name]
+puts person[:age]
+puts person[:handsome]
