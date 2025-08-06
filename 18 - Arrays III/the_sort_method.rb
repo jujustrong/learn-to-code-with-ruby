01bb4afc4ca325a73389f2386ddbd5f7992737e6
Julian Armstrong <julian.armstrong8@gmail.com>
2025-08-06 16:02:28 -0600

    completed "sort method" lesson

---
+++ 18 - Arrays III/the_sort_method.rb
@@ -0,0 +1,9 @@
+# sort method - sorts an array in ascending/descending order
+
+p [2,3,1].sort
+p [2,3,1].sort.reverse
+
+p ["Hello", "Blah", "Zebra"].sort
+p ["Hello", "Blah", "Zebra"].sort.reverse
+p ["Hello", "zebra", "Blah", "Zebra", "artists"].sort  # Ruby considers all capital letters to come before lowercase letters
+
