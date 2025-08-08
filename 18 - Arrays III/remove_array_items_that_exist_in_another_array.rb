e1beabff90320b8158d9adb3a9aa4eae39a29a39
Julian Armstrong <julian.armstrong8@gmail.com>
2025-08-08 13:45:54 -0600

    completed "remove dupes with minus" lesson

---
+++ 18 - Arrays III/remove_array_items_that_exist_in_another_array.rb
@@ -0,0 +1,4 @@
+# - sign - removes all array elements found in second array
+
+p [1, 1, 2, 2, 3, 3, 3, 4, 5] - [2, 3] # this is saying remove all occurences of 2 and 3 from the first array
+p [1, 1, 2, 2, 3, 3, 3, 4, 5].-([2, 3])
