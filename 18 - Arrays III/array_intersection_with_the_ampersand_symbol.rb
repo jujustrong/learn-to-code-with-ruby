76e697fcde2eda4f5b2889d8755af675fcd3ad6d
Julian Armstrong <julian.armstrong8@gmail.com>
2025-08-08 13:50:14 -0600

    added comment to intersection lesson

--- 18 - Arrays III/array_intersection_with_the_ampersand_symbol.rb
+++ 18 - Arrays III/array_intersection_with_the_ampersand_symbol.rb
@@ -3,3 +3,5 @@
 p [1, 1, 2, 3, 4, 5] & [1, 4, 5, 8, 9]
 p [1, 1, 2, 3, 4, 5].&([1, 4, 5, 8, 9])
 p [1, 1, 2, 3, 4, 5] & [1, 4, 5, 8, 9] & [ 4, 5, 10, 11]
+
+# has to be found in each array to be included in the final output
