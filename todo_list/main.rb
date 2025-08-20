3d62d8b76971048993620a123a4e99f373b0104c
Julian Armstrong <julian.armstrong8@gmail.com>
2025-08-19 23:51:21 -0600

    Adding conditionals for task choice

--- todo_list/main.rb
+++ todo_list/main.rb
@@ -8,10 +8,17 @@ def todo
   tasks_to_complete = []
   completed_tasks = []
   puts "-"*40
-  puts "What would you like to do?"
-  choice = ask("1: Add new task\n2: Mark Task Completed\n3: View Tasks\n4: View Completed Tasks\n5: Exit\nEnter number (1-5): ")
+  loop do
+    puts "What would you like to do?"
+    choice = ask("1: Add new task\n2: Mark Task Completed\n3: View Tasks\n4: View Completed Tasks\n5: Exit\nEnter Number (1-5): ")
+    if choice == 1
+      new_task = ask("What task would you like to add? ")
+      tasks_to_complete << new_task.capitalize
+      puts "Task Added!"
+    elsif choice == 2
+      puts tasks_to_complete
 
-  
+    end
 end
 
 todo
