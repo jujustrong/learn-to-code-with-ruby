f9c28bc7fb7bf84853c50b61551209bfd8a365e8
Julian Armstrong <julian.armstrong8@gmail.com>
2025-08-22 00:12:07 -0600

    Refactored if statement to case statement for readability

--- todo_list/main.rb
+++ todo_list/main.rb
@@ -7,20 +7,26 @@ end
 def todo
   tasks_to_complete = []
   completed_tasks = []
-  counter = 0
 
   puts "-"*40
   loop do
     puts "What would you like to do?"
-    choice = ask("1: Add new task\n2: Mark Task Completed\n3: View Tasks\n4: View Completed Tasks\n5: Exit\nEnter Number (1-5): ")
-    if choice == 1
+    choice = ask("1: Add new task\n2: Mark Task Completed\n3: View Tasks\n4: View Completed Tasks\n5: Exit\nEnter Number (1-5): ").to_i
+
+    case choice
+    when 1
       new_task = ask("What task would you like to add? ")
       tasks_to_complete << new_task.capitalize
       puts "Task Added!"
-    elsif choice == 2
-      completed_tasks.map { |task| puts "#{counter}: #{task}, "  }
+    
+    when 2
+      puts "No tasks to complete!" if tasks_to_complete.empty?
+      
+      puts "Which task would you like to mark as completed?"
+      puts tasks_to_complete.each_with_index { |task, index| puts "#{index + 1}: #{task}" }
+
+    
 
     end
-end
 
-todo
+end
