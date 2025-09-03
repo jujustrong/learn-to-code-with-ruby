ef83f5deba02b93a596897382d9f3d489f658cce
Julian Armstrong <julian.armstrong8@gmail.com>
2025-09-03 13:05:21 -0600

    completed declare instance variables coding exercise

---
+++ 20 - Classes I/coding_exercise_declare_instance_variables.rb
@@ -0,0 +1,14 @@
+# Declare a Musical class that includes "name",
+# "cast", and "duration" instance variables.
+# Feel free to initialize the instance variables
+# to whatever values you'd like.
+
+class Musical
+
+  def initialize
+    @name = "La La Land" 
+    @cast = ["Ryan Gosling", "Emma Stone"]
+    @duration = 140
+  end
+
+end
