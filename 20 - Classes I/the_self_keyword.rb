5001fe00aec4042b07bec0b329a17b10d9007fcd
Julian Armstrong <julian.armstrong8@gmail.com>
2025-09-04 15:14:03 -0600

    completed "the self keyword" lesson

---
+++ 20 - Classes I/the_self_keyword.rb
@@ -0,0 +1,25 @@
+# self keyword - returns the entity in which the keyword is used
+
+class Guitar
+  puts "Inside Guitar class: #{self}"
+
+  def initialize
+    @type = "Acoustic"
+    @wood = "Alder"
+    @strings = 6
+  end
+
+  def to_s
+    "An #{@type} #{@wood} guitar with #{@strings} strings."
+  end
+
+  def details
+    # puts "Inside details instance method: #{self}"
+    puts "Is it nil? #{self.nil?}. It is made from the #{self.class} class."
+  end
+
+end
+
+guitar = Guitar.new
+# guitar.details #self refers to the instance itself. Self refers to the guitar.
+guitar.details
