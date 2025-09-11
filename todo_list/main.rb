beb9a592e2645b8b0600a490ff5f7e3ce2680c6d
Julian Armstrong <julian.armstrong8@gmail.com>
2025-09-11 00:04:42 -0600

    Added system clears and moved flow into class methods instead of in the main menu logic

--- todo_list/main.rb
+++ todo_list/main.rb
@@ -13,6 +13,7 @@ class Task
   end
 
   def mark_completed
+    puts "Task Completed!"
     @completed = true
   end
 
@@ -31,12 +32,20 @@ class TodoList
 
   def add_task(description)
     @tasks << Task.new(description)
-
+    puts "Task Added!"
+    sleep(2)
+    system('clear')
   end  
 
   def mark_task_completed(index)
     task = @tasks[index]
-    task.mark_completed if task
+    if task
+      task.mark_completed
+      system('clear')
+    else
+      puts "Task not in ToDo List!"
+      system('clear')
+    end
   end
 
   def view_tasks
@@ -44,13 +53,15 @@ class TodoList
     @tasks.each_with_index do |task, i|
       puts "#{i}: #{task}"
     end
+    puts
   end
 
   def view_completed_tasks
-    puts "Complete Tasks:"
+    puts "Completed Tasks:"
     @tasks.each_with_index do |task, i|
       puts "#{i}: #{task}" if task.completed
     end
+    puts
   end
 
 end
@@ -62,23 +73,16 @@ def run
   puts "-"*40
   loop do
     puts "What would you like to do?"
-    choice = ask(
-      "1: Add new task\n
-      2: Mark Task Completed\n
-      3: View Tasks\n
-      4: View Completed Tasks\n
-      5: Exit\nEnter Number (1-5): "
-      ).to_i
+    choice = ask("1: Add new task\n2: Mark Task Completed\n3: View Tasks\n4: View Completed Tasks\n5: Exit\nEnter Number (1-5): ").to_i
     system('clear')
 
     case choice
 
     when 1
-      list.add_task(ask("What task would you like to add? "))
-
+      list.add_task(ask("Enter New Task: "))
     when 2
       list.view_tasks
-      list.mark_task_completed(ask("Enter task number to mark completed: "))
+      list.mark_task_completed(ask("Enter task number to mark completed: ").to_i)
 
     when 3
       if list.nil?
@@ -91,7 +95,9 @@ def run
       list.view_completed_tasks
 
     when 5
-      puts "Exiting To-Do List. Goodbye!"
+      print "Goodbye!"
+      sleep(2.5)
+      system('clear')
       break
 
     else
