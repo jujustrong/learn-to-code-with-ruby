16a86cab5e4e35ed8181fa556d150621688ae6b7
Julian Armstrong <julian.armstrong8@gmail.com>
2025-09-20 23:08:20 -0600

    added Enemy class with 3 methods, alive, take_damage, and attack

---
+++ Into_The_Woods/enemy.rb
@@ -0,0 +1,27 @@
+class Enemy
+  attr_accessor :name, :health, :attacks
+
+  def initialize(name, health, attacks)
+    @name = name
+    @health = health
+    @attacks = attacks
+  end
+  
+  def alive?
+    @health > 0
+  end
+
+  def take_damage(amount)
+    @health -= amount
+    @health = 0 if @health < 0
+    puts "#{@name} took #{amount} damage! (Health: #{@health})"
+  end
+
+  def attack(player)
+    attack_name, damage_range = @attacks.to_a.sample
+    damage = rand(damage_range)
+    puts "#{@name} uses #{attack_name}! It deals #{damage} damage!"
+    player.take_damage(damage)
+  end
+
+end
