a7543d2f4ab0499c6146fd0f19b16715437bb971
Julian Armstrong <julian.armstrong8@gmail.com>
2025-09-25 00:52:47 -0600

    added initial method skeletons to game class

--- harry_potter_adventure.rb/game.rb
+++ harry_potter_adventure.rb/game.rb
@@ -1,7 +1,43 @@
+require_relative "player"
+require_relative "npc"
+require_relative "spell"
+require_relative "duel"
+require_relative "location"
+require_relative "event"
+require_relative "item"
+require_relative "inventory"
+require_relative "utility"
+
 class Game
   
   def initialize
+    @player = nil
+    @running = true
+  end
+
+  def start
     
   end
 
+  def create_player
+    
+  end
+
+  def main_menu
+    
+  end
+
+  def explore
+    
+  end
+
+  def quit
+    
+  end
+
+
+
+
 end
+
+Game.new.start
