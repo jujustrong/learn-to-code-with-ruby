9e7f0a0b5e0def127bcd5170e1b7481253c43d76
Julian Armstrong <julian.armstrong8@gmail.com>
2025-09-25 01:42:31 -0600

    created create player, choose house, main menu, explore, and quit methods in main game file

--- harry_potter_adventure.rb/game.rb
+++ harry_potter_adventure.rb/game.rb
@@ -16,23 +16,63 @@ class Game
   end
 
   def start
-    
+    clear_screen
+    puts "⚡️ Welcome to Hogwarts RPG ⚡️"
+    puts 
+    create_player
+    main_menu
   end
 
   def create_player
-    
+    name = ask("What is your name? ")
+    puts "Hello #{name} nice to meet you!"
+    sleep(3)
+    choose_house
+    @player = Player.new(name, house)
+  end
+
+  def choose_house
+    houses = ["gryffindor", "slytherin", "ravenclaw", "hufflepuff"]
+    loop do
+      house = ask("Choose your house (Gryffindor 🦁, Slytherin 🐍, Ravenclaw 🦉, Hufflepuff 🦡): ").downcase.strip
+      if houses.include?(house)
+        house.capitalize
+      else
+        puts "That's not a valid house. Maybe check your spelling!"
+      end
+
+    end
+
   end
 
   def main_menu
-    
+    while @running
+      puts "---- Main Menu ----"
+      puts "1. Explore 🏰"
+      puts "2. Inventory 🧳"
+      puts "3. Quit 👋"
+      choice = ask("Enter 1, 2, or 3: ")
+
+      case choice
+      when "1" then explore
+      when "2" then @player.show_inventory
+      when "3" then quit
+      else
+        puts "Invalid choice."
+      end
+
+    end
+
   end
 
   def explore
-    
+    # Placeholder until i put in explore functionality
+    puts "You wander through the castle halls..."
   end
 
   def quit
-    
+    puts "Goodbye Wizard! 🪄"
+    @running = false
   end
 
 
