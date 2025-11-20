4f80eb34c41917ec0265fbb343ef55d7c9fc2d5e
Julian Armstrong <julian.armstrong8@gmail.com>
2025-11-20 13:49:48 -0700

    completed "require and require_relative methods" lesson

---
+++ 24 - Input and Output/the_require_and_require_relative_methods.rb
@@ -0,0 +1,20 @@
+# Require does not look into the directory first, it looks in the ruby instillation directory first.
+# It imports from Ruby's import database first
+# It only loads something once instead of several times like when we use load
+# require_relative looks in the current directory first
+
+puts "Welcome to the program"
+
+require_relative "another_file.rb"
+require_relative "another_file.rb"
+require_relative "another_file.rb"
+
+some_method
+table = Table.new
+puts table.class
+
+# require "./another_file.rb"
+# require "./another_file.rb" if 8 > 5
+
+
+# puts "We've reached the end of our program."
